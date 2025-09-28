ec3df5b3925398794ac8884ff265ef29ac9fabe5
KAZUHIRO JINMA <k-jinma@uhe.ac.jp>
2025-09-29 00:50:46 +0900

    初期設定ファイルとSpring Bootアプリケーションの作成。Gradleビルド設定、.gitignore、.gitattributes、アプリケーションのメインクラス、テストクラス、プロパティファイルを追加。

---
+++ src/main/java/com/example/schoolfes_reserve/SchoolfesReserveApplication.java
@@ -0,0 +1,13 @@
+package com.example.schoolfes_reserve;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolfesReserveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchoolfesReserveApplication.class, args);
+	}
+
+}
